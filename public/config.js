eb14f3943c3dc6a300e8e93ef44a264a42a578f4
Mario Reder <mario.reder@pm.me>
2025-10-09 10:44:04 +0200

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -25,7 +25,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_TWITTER_URL": "",
   "VITE_SEO_SITE_NAME": "Rove DEX",
   "VITE_SEO_SITE_DESCRIPTION": "",
-  "VITE_SEO_SITE_URL": "https://dex.orderly.network/rove-4737/",
+  "VITE_SEO_SITE_URL": "https://dex.rove.now",
   "VITE_SEO_SITE_LANGUAGE": "en",
   "VITE_SEO_SITE_LOCALE": "en_US",
   "VITE_SEO_TWITTER_HANDLE": "@whiteliecrypto",
